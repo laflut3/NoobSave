bfacfe8fedaa4237f2ae583ded079f7ab979ad03
jean-mulet <leo0609leo@gmail.com>
2024-12-12 14:02:50 +0100

    feat(): ajout dependance spring security et config spring secure

---
+++ .NoobSave_back/src/main/java/NoobSave/_L/garcia/NoobSave/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package NoobSave._L.garcia.NoobSave.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return
+                httpSecurity
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/fichiers/**").permitAll() // Autoriser les routes fichiers
+                                .requestMatchers("/api/users/**").permitAll() // Autoriser la connexion
+                                .anyRequest().authenticated() // Toutes les autres routes nécessitent une authentification
+                        )
+                        .build();
+    }
+}
+
