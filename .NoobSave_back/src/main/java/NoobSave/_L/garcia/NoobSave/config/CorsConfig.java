b9f6ed49f66f5bb8cccb0e08154c0641107015ac
jean-mulet <leo0609leo@gmail.com>
2024-12-24 15:00:50 +0100

    feat(): bean de configuration web pour le filtrage

--- .NoobSave_back/src/main/java/NoobSave/_L/garcia/NoobSave/config/CorsConfig.java
+++ .NoobSave_back/src/main/java/NoobSave/_L/garcia/NoobSave/config/CorsConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public CorsFilter corsFilter() {
@@ -21,5 +23,14 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Autoriser toutes les routes
+                .allowedOrigins("http://localhost:3000") // Autoriser localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Autoriser les méthodes nécessaires
+                .allowedHeaders("*")
+                .allowCredentials(true); // Autoriser les cookies (si nécessaire)
     }
 }
